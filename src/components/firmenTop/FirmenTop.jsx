e084aa76dbbfe371c69ceee76509d0dcbf53ef92
Tanvir Chowdhury <50425710+TanvirChy@users.noreply.github.com>
2022-01-25 17:58:30 +0600

    add react-select library

--- src/components/firmenTop/FirmenTop.jsx
+++ src/components/firmenTop/FirmenTop.jsx
@@ -1,19 +1,15 @@
-import React, { useState } from 'react';
-import Select from 'react-select';
+import React, { useState } from "react";
+import Select from "react-select";
 
 import "./firmenTop.css";
 
 const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' },
+  { value: "chocolate", label: "Chocolate" },
+  { value: "strawberry", label: "Strawberry" },
+  { value: "vanilla", label: "Vanilla" },
 ];
 
-
 const FirmenTop = ({ ergebnisse, setErgebnisse }) => {
-  const handleChangeErgebnisse = (e) => {
-    setErgebnisse(e.target.value);
-  };
 
   return (
     <div className="firmen_top_content">
@@ -25,23 +21,16 @@ const FirmenTop = ({ ergebnisse, setErgebnisse }) => {
       </div>
       <div className="input_section">
         <div className="input_select">
-          <label htmlFor="" className="input_label">
+          <label className="input_label">
             Ergebnisse
           </label>
 
-          <select
-            className="input_select_feild"
-            value={ergebnisse}
-            onChange={handleChangeErgebnisse}
-          >
-            <option value="hello">hello</option>
-            <option value="Ergebnisse">Ergebnisse</option>
-            <option value="hello">hello</option>
-            <option value="Ergebnisse">Ergebnisse</option>
-            <option value="hello">hello</option>
-            <option value="Suchtext">Suchtext</option>
-            <option value="hello">hello</option>
-          </select>
+          <Select
+            defaultValue={ergebnisse}
+            onChange={setErgebnisse}
+            
+            options={options}
+          />
         </div>
       </div>
     </div>
