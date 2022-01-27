a0d17d5ab701b5ffc1edf124da23a15f7146761a
Tanvir Chowdhury <50425710+TanvirChy@users.noreply.github.com>
2022-01-27 13:19:11 +0600

    all company fached done

--- src/components/firmenTop/FirmenTop.jsx
+++ src/components/firmenTop/FirmenTop.jsx
@@ -1,40 +1,79 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Select from "react-select";
-
+import axios from "axios";
 import "./firmenTop.css";
 
-const options = [
-  { value: "chocolate", label: "Chocolate" },
-  { value: "strawberry", label: "Strawberry" },
-  { value: "dhaka", label: "dhaka" },
-  { value: "chittagong", label: "chittagong" },
-  { value: "sylet", label: "sylet" },
-  { value: "rajshahi", label: "rajshahi" },
-  { value: "rangpur", label: "rangpur" },
-  { value: "bogura", label: "bogura" },
-  
-];
+const options = [];
 
 const FirmenTop = ({ ergebnisse, setErgebnisse }) => {
-  const [companyName,setCompanyName] = useState('Swisscom');
+  const [companyName, setCompanyName] = useState("Swisscom");
+
+  const [companyNames, setCompanyNames] = useState([]);
+  let data = companyName;
+
+  useEffect(() => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        // // 'Access-Control-Allow-Origin': true,
+        // 'Access-Control-Allow-Origin': '*',
+        // "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+      },
+      auth: {
+        username: "rg@multi-concept.ch",
+        password: "4z7P6NZK",
+      },
+    };
+    const sendCompanyName = async () => {
+      const response = await axios.post("http://localhost:5000", data, config);
+      setCompanyNames(response.data);
+      console.log('response',response.data);
+    };
+    sendCompanyName();
+  }, [data]);
+
+  // useEffect(()=>{
+  //   const getCompanyData = async () => {
+  //     const response = await axios.get(
+  //       "http://localhost:6000"
+  //     )
+  //     console.log(response);
+  //   }
+  //   getCompanyData()
+  // },[])
+
+  //  { value: 'chocolate', label: 'Chocolate' },
+
+  const makeOption = () => {
+    companyNames.map((company) => {
+      options.push({ label: `${company.name}` , ...company });
+    });
+  };
+  makeOption();
+  console.log(options);
+
   return (
     <div className="firmen_top_content">
       <div className="input-section-first">
-        <label  className="input-first-label">
-          Suchtext
-        </label>
-        {/* <input type="text" onChange={(e)=>setCompanyName(e.target.value)} value={companyName} /> */}
-        <div className="input-value">Swisscom</div>
+        <label className="input-first-label">Suchtext</label>
+        <input
+          type="text"
+          onChange={(e) => setCompanyName(e.target.value)}
+          value={companyName}
+        />
       </div>
       <div className="input-section-second">
         <label className="input-second-label">Ergebnisse</label>
 
-        <Select
-          defaultValue={ergebnisse}
-          onChange={setErgebnisse}
-          className="input-value"
-          options={options}
-        />
+        {companyNames && (
+          <Select
+            defaultValue={ergebnisse}
+            onChange={setErgebnisse}
+            className="input-value"
+            options={options}
+          />
+        )}
+        {console.log(ergebnisse)}
       </div>
     </div>
   );
